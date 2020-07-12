69fdcea17dbdd642f59b7b0b4db81261182b440a
abhishekkrthakur <abhishek4@gmail.com>
2020-07-12 17:48:28 +0200

    fix dataset

--- src/dataset.py
+++ src/dataset.py
@@ -33,29 +33,29 @@ class EntityDataset:
             target_pos.extend([pos[i]] * input_len)
             target_tag.extend([tags[i]] * input_len)
 
-            ids = ids[:config.MAX_LEN - 2]
-            target_pos = target_pos[:config.MAX_LEN - 2]
-            target_tag = target_tag[:config.MAX_LEN - 2]
+        ids = ids[:config.MAX_LEN - 2]
+        target_pos = target_pos[:config.MAX_LEN - 2]
+        target_tag = target_tag[:config.MAX_LEN - 2]
 
-            ids = [101] + ids + [102]
-            target_pos = [0] + target_pos + [0]
-            target_tag = [0] + target_tag + [0]
+        ids = [101] + ids + [102]
+        target_pos = [0] + target_pos + [0]
+        target_tag = [0] + target_tag + [0]
 
-            mask = [1] * len(ids)
-            token_type_ids = [0] * len(ids)
+        mask = [1] * len(ids)
+        token_type_ids = [0] * len(ids)
 
-            padding_len = config.MAX_LEN - len(ids)
+        padding_len = config.MAX_LEN - len(ids)
 
-            ids = ids + ([0] * padding_len)
-            mask = mask + ([0] * padding_len)
-            token_type_ids = token_type_ids + ([0] * padding_len)
-            target_pos = target_pos + ([0] * padding_len)
-            target_tag = target_tag + ([0] * padding_len)
+        ids = ids + ([0] * padding_len)
+        mask = mask + ([0] * padding_len)
+        token_type_ids = token_type_ids + ([0] * padding_len)
+        target_pos = target_pos + ([0] * padding_len)
+        target_tag = target_tag + ([0] * padding_len)
 
-            return {
-                "ids": torch.tensor(ids, dtype=torch.long),
-                "mask": torch.tensor(mask, dtype=torch.long),
-                "token_type_ids": torch.tensor(token_type_ids, dtype=torch.long),
-                "target_pos": torch.tensor(target_pos, dtype=torch.long),
-                "target_tag": torch.tensor(target_tag, dtype=torch.long),
-            }
+        return {
+            "ids": torch.tensor(ids, dtype=torch.long),
+            "mask": torch.tensor(mask, dtype=torch.long),
+            "token_type_ids": torch.tensor(token_type_ids, dtype=torch.long),
+            "target_pos": torch.tensor(target_pos, dtype=torch.long),
+            "target_tag": torch.tensor(target_tag, dtype=torch.long),
+        }
